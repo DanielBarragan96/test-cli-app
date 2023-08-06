c2dc6e7c2151c3f8aaac05242ed9aad541945530
DanielBarragan96 <ie702554@iteso.mx>
2023-08-06 17:48:27 -0600

    implementation with selection and spinner

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-import { intro, outro } from "@clack/prompts";
+import { intro, outro, select, spinner } from "@clack/prompts";
 
-intro(`create-my-app`);
-// Do stuff
-outro(`You're all set!`);
+intro(`Muestra example`);
+
+const projectType = await select({
+  message: "Pick a project type.",
+  options: [
+    {
+      value: "GEN_EXCEL",
+      label: "GEN_EXCEL",
+      hint: "generate from excel file",
+    },
+    {
+      value: "GEN_MONGODB",
+      label: "GEN_MONGODB",
+      hint: "generate from mongodb",
+    },
+    {
+      value: "EXCEL2MONGODB",
+      label: "EXCEL2MONGODB",
+      hint: "export to mongodb",
+    },
+    {
+      value: "ENOBRA2MONGODB",
+      label: "ENOBRA2MONGODB",
+      hint: "export to mongodb",
+    },
+  ],
+});
+
+const s = spinner();
+s.start("Working on it...");
+await new Promise((resolve) => {
+  setTimeout(resolve, 1000);
+});
+s.stop("Generated files");
+outro(`Done!`);
